29a63d7e49a40085e563a1381f3627ce3f96e97f
Dinithi Himaya <as20240455@sci.sjp.ac.lk>
2026-07-23 14:30:50 +0530

    update api.js

--- frontend-development/js/api.js
+++ frontend-development/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api";
+const API_URL = 'http://10.33.9.243:8080/api';
 
 /* ==========================================================
    COMMON REQUEST FUNCTION
